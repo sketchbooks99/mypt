9bfa6de1a161fcc3ab5248a08b1adb947ad4acbc
Kiuchi Shunji <lunaearth445@gmail.com>
2020-10-31 00:15:27 +0900

    Add constructor of Image class

--- src/core/Image.h
+++ src/core/Image.h
@@ -4,36 +4,48 @@
 #include <vector>
 #include "../core/Util.h"
 
-// Should RGB value be implemented by std::vector? 
-// Each pixel have channel size vector? I have no good idea to implement. 
-template <typename T>
-struct Pixel {
-    T get() { return data; }
-    void set(T val) { 
-        if(sizeof(val) != this->val)
-            throw std::runtime_error("The number of channel is different!\n");
-        this->val = val; 
-    }
-private:
-    T val; // pixel value 
-};
+// // Should RGB value be implemented by std::vector? 
+// // Each pixel have channel size vector? I have no good idea to implement. 
+// template <typename T>
+// struct Pixel {
+//     const T get() { return val; }
+//     const void set(T val) {
+//         this->val = val; 
+//     }
+// private:
+//     T val; // pixel value 
+// };
 
+// PixelType should be float(int) or float(int)3 or float(int)4
+template <typename PixelType>
 class Image {
 public:
-    enum class PixelType{ RGB, RGBA, GRAY };
-    Image(int width, int height, PixelType type = PixelType::RGB);
+    // enum class PixelType{ RGB, RGBA, GRAY };
+    Image(int width, int height);
+    void set(int x, int y, PixelType val) {
+        assert(data.size() < x && data[x].size() < y);
+        data[x][y] = val;
+    }
+    Pixeltype get(int x, int y)
+    {
+        assert(data.size() < x && data[x].size() < y);
+        return data[x][y];
+    }
 private:
-    void allocate(int width, int height, PixelType type);
-
+    std::vector<std::vector<PixelType>> data;
     int width, height;
 };
 
-Image::Image(int width, int height, PixelType type) {
-
-}
-
-void Image::allocate(int width, int height, PixelType type) {
-    
+template <typename PixelType>
+Image<PixelType>::Image(int width, int height) {
+    if(width <= 0 || height <= 0)
+        throw std::runtime_error("Image class must have at least 1 pixel!\n");
+        
+    data.resize(width);
+    for(auto &d : data)
+    {
+        d.resize(height);
+    }
 }
 
 #endif
