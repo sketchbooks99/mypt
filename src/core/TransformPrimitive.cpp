cf6d044d5ee887be50cff3d6be43616af1a2be93
Shunji Kiuchi <lunaearth445@gmail.com>
2020-11-28 14:33:37 +0900

    Update TransformPrimitive

--- src/core/TransformPrimitive.cpp
+++ src/core/TransformPrimitive.cpp
@@ -3,8 +3,14 @@
 // ----------------------------------------------------------------------
 bool TransformPrimitive::intersect(const Ray& r, double t_min, double t_max, HitRecord& rec) const {
     // transformed ray
+    Ray tr = transform_ray(r, this->mat);
+    if(!p->intersect(tr, t_min, t_max, rec))
+        return false;
+    
+    return true;
 }
 
+// ----------------------------------------------------------------------
 AABB TransformPrimitive::bounding() const {
     auto min = p->bounding().min();
     auto max = p->bounding().max();
@@ -16,46 +22,69 @@ AABB TransformPrimitive::bounding() const {
     return AABB(tr_min, tr_max);
 }
 
+// ----------------------------------------------------------------------
 void TransformPrimitive::rotate_x(double theta) {
     double c = cos(theta);
     double s = sin(theta);
-    this->mat = this->mat * mat4(1, 0, 0, 0,
+    this->mat = this->mat * mat4(1, 0, 0,  0,
                                  0, c, -s, 0,
-                                 0, s, c, 0,
-                                 0, 0, 0, 1);
+                                 0, s, c,  0,
+                                 0, 0, 0,  1);
 }
 void TransformPrimitive::rotate_y(double theta) {
     double c = cos(theta);
     double s = sin(theta);
-    this->mat = this->mat * mat4(c, 0, s, 0, 
-                                 0, 1, 0, 0,
+    this->mat = this->mat * mat4(c,  0, s, 0, 
+                                 0,  1, 0, 0,
                                  -s, 0, c, 0,
-                                 0, 0, 0, 1);
+                                 0,  0, 0, 1);
 }
 void TransformPrimitive::rotate_z(double theta) {
     double c = cos(theta);
     double s = sin(theta);
     this->mat = this->mat * mat4(c, -s, 0, 0, 
-                                 s, c, 0, 0,
-                                 0, 0, 1, 0,
-                                 0, 0, 0, 1);
+                                 s, c,  0, 0,
+                                 0, 0,  1, 0,
+                                 0, 0,  0, 1);
 }
 void TransformPrimitive::rotate(double theta, const vec3& axis) {
-
+    double sx = sin(theta) * axis.x;
+    double cx = cos(theta) * axis.x;
+    double sy = sin(theta) * axis.y;
+    double cy = cos(theta) * axis.y;
+    double sz = sin(theta) * axis.z;
+    double cz = cos(theta) * axis.z;
+    this->mat = this->mat * mat4(
+        cy*cz, sx*sy*cz-sz*cx, sx*sz+cx*sy*cz, 0,
+        cy*sz, cx*cz+sz*sy*sz, cx*sy*sz-sx*cz, 0,
+        -sy,   sx*cy,          cx*cy,          0,
+        0,     0,              0,              1
+    );
 }
 
+// ----------------------------------------------------------------------
 void TransformPrimitive::translate(const vec3& t) {
-
+    this->mat.mat[0][3] += t.x;
+    this->mat.mat[1][3] += t.y;
+    this->mat.mat[2][3] += t.z;
 }
 
+// ----------------------------------------------------------------------
 void TransformPrimitive::scale(const vec3& s) {
-
+    this->mat = this->mat * mat4(s.x, 0,   0,   0,
+                                 0,   s.y, 0,   0,
+                                 0,   0,   s.z, 0,
+                                 0,   0,   0,   1);
 }
 void TransformPrimitive::scale(double s) {
-
+    this->mat = this->mat * mat4(s, 0, 0, 0,
+                                 0, s, 0, 0,
+                                 0, 0, s, 0,
+                                 0, 0, 0, 1);
 }
 
-Ray transform(const mat4& mat, const Ray& r) {
+// ----------------------------------------------------------------------
+Ray transform_ray(const Ray& r, const mat4& mat) {
     auto ro = mat * r.origin();
     auto rd = mat * r.direction();
     return Ray(ro, rd, r.time());
--- src/core/TransformPrimitive.h
+++ src/core/TransformPrimitive.h
@@ -4,6 +4,7 @@
 #include "mat4.h"
 
 class TransformPrimitive final : public Primitive {
+public:
     TransformPrimitive(std::shared_ptr<Primitive> p) : p(p), mat(mat4()) {}
     TransformPrimitive(std::shared_ptr<Primitive> p, mat4 mat) : p(p), mat(mat.mat) {}
     bool intersect(const Ray& r, double t_min, double t_max, HitRecord& rec) const override;
@@ -24,5 +25,5 @@ private:
     mat4 mat;
 };
 
-Ray transform(const mat4& mat, const Ray& r);
+Ray transform(const Ray& r, const mat4& mat);
 
