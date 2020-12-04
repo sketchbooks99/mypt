d67a7585ffd2da0f0e597d86500e8e553bb1aa0e
Shunji Kiuchi <lunaearth445@gmail.com>
2020-12-04 21:51:06 +0900

    Minor update

--- scene/planeandbunny.h
+++ scene/planeandbunny.h
@@ -83,7 +83,7 @@ std::vector<std::shared_ptr<Primitive>> scene() {
     ts.rotateY(pi/4.f);
     ts.scale(50.0f);
 
-    albedo = vec3(0.05, 0.80, 0.05);
+    albedo = vec3(0.80, 0.80, 0.05);
     bunny_lambert = std::make_shared<Lambertian>(albedo);
     bunny = createTriangleMesh("data/model/bunny.obj", vec3(0.0f), 1.0f, vec3(1,1,1), true);
     bunny_transform = std::make_shared<Transform>(ts.getCurrentTransform());
