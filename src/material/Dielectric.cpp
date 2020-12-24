edfa72df843b62b9337efb1048e00e35bd4923d2
Shunji Kiuchi <lunaearth445@gmail.com>
2020-12-25 02:19:10 +0900

    Ray never interact from in sphere to out sphere

--- src/material/Dielectric.cpp
+++ src/material/Dielectric.cpp
@@ -3,7 +3,7 @@
 
 namespace mypt {
 
-/* 
+#if 1
 bool Dielectric::scatter(
     const Ray& r_in, HitRecord& rec, ScatterRecord& srec
 ) const  {
@@ -15,7 +15,8 @@ bool Dielectric::scatter(
     vec3 refracted;
     float ni_over_nt;
     float cosine;
-    if(dot(r_in.direction(), rec.normal) > 0) {
+    bool into = dot(r_in.direction(), rec.normal) < 0;
+    if(!into) {
         outward_normal = -rec.normal;
         ni_over_nt = ref_idx;
         cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
@@ -26,20 +27,28 @@ bool Dielectric::scatter(
     }
     
     float reflect_prob;
-    if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+    if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) 
         reflect_prob = schlick(cosine, ref_idx);
-    } else {
+    else 
         reflect_prob = 1.0;
-    }
+    
+    vec3 direction;
+    bool is_reflect;
     if(random_double() < reflect_prob) {
         srec.specular_ray = Ray(rec.p, reflected);
+        is_reflect = true;
+        direction = reflected;
     } else {
         srec.specular_ray = Ray(rec.p, refracted);
+        is_reflect = false;
+        direction = refracted;
     }
+
+    std::cout << into << ", " << rec.p << ", " << r_in.direction() << ", " << direction << ", is_reflect: " << is_reflect << std::endl;
+    
     return true;
 } 
-*/
-
+#else
 bool Dielectric::scatter(
     const Ray& r_in, HitRecord& rec, ScatterRecord& srec
 ) const  {
@@ -62,5 +71,7 @@ bool Dielectric::scatter(
         srec.specular_ray = Ray(rec.p, refract(r_in.direction(), outward_normal, ni_over_nt));
     return true;
 }
+#endif
+
 
 }
