59ed7e7d286282c9225829e07327e86e9e649f70
Kiuchi Shunji <lunaearth445@gmail.com>
2021-01-28 02:45:32 +0900

    Calculate AABB of ShapePrimitive in constructor

--- src/core/Primitive.cpp
+++ src/core/Primitive.cpp
@@ -12,6 +12,32 @@
 
 namespace mypt {
 
+ShapePrimitive::ShapePrimitive(
+    std::shared_ptr<Shape> shape, std::shared_ptr<Material> material, std::shared_ptr<Transform> transform
+) : shape(shape), material(material), transform(transform)
+{
+    vec3 min(infinity, infinity, infinity);
+    vec3 max(-infinity, -infinity, -infinity);
+
+    for(int i=0; i<2; i++) {            // 0: min.x, 1: max.x
+        for(int j=0; j<2; j++) {        // 0: min.y, 1: max.y
+            for(int k=0; k<2; k++) {    // 0: min.z, 1: max.z
+                auto x = (1-i)*shape->bounding().min().x + i*shape->bounding().max().x;
+                auto y = (1-j)*shape->bounding().min().y + j*shape->bounding().max().y;
+                auto z = (1-k)*shape->bounding().min().z + k*shape->bounding().max().z;
+                
+                vec3 tr_corner = mat4::point_mul(transform->getMatrix(), vec3(x, y, z));
+                for(int l=0; l<3; l++) {
+                    min[l] = fmin(min[l], tr_corner[l]);
+                    max[l] = fmax(max[l], tr_corner[l]);
+                }
+            } 
+        }
+    }
+
+    bbox = AABB(min, max);
+}
+
 // ShapePrimitive ----------------------------------------------------------------------
 bool ShapePrimitive::intersect(const Ray& r, double t_min, double t_max, HitRecord& rec) const {
     Ray tr_ray = *transform * r;
@@ -32,26 +58,7 @@ bool ShapePrimitive::intersect(const Ray& r, double t_min, double t_max, HitReco
 }
 
 AABB ShapePrimitive::bounding() const {
-    vec3 min(infinity, infinity, infinity);
-    vec3 max(-infinity, -infinity, -infinity);
-
-    for(int i=0; i<2; i++) {            // 0: min.x, 1: max.x
-        for(int j=0; j<2; j++) {        // 0: min.y, 1: max.y
-            for(int k=0; k<2; k++) {    // 0: min.z, 1: max.z
-                auto x = (1-i)*shape->bounding().min().x + i*shape->bounding().max().x;
-                auto y = (1-j)*shape->bounding().min().y + j*shape->bounding().max().y;
-                auto z = (1-k)*shape->bounding().min().z + k*shape->bounding().max().z;
-                
-                vec3 tr_corner = mat4::point_mul(transform->getMatrix(), vec3(x, y, z));
-                for(int l=0; l<3; l++) {
-                    min[l] = fmin(min[l], tr_corner[l]);
-                    max[l] = fmax(max[l], tr_corner[l]);
-                }
-            } 
-        }
-    }
-
-    return AABB(min, max);
+    return bbox;
 }
 
 double ShapePrimitive::pdf_value(const vec3& o, const vec3& v) const {
